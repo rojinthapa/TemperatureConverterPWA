224c9a7117046933df956e99e0ec4cf23d113757
rojinthapa <rojinthapa66@gmail.com>
2025-10-30 11:06:29 +0000

    Create converter.js

---
+++ converter.js
@@ -0,0 +1,36 @@
+const inputField = document.getElementById('input-temp');
+const fromUnitField = document.getElementById('input-unit');
+const toUnitField = document.getElementById('output-unit');
+const outputField = document.getElementById('output-temp');
+const form = document.getElementById('converter');
+
+function convertTemp(value, fromUnit, toUnit) {
+  if (isNaN(value)) return NaN;
+  if (fromUnit === 'c') {
+    if (toUnit === 'f') return value * 9 / 5 + 32;
+    if (toUnit === 'k') return value + 273.15;
+    return value;
+  }
+  if (fromUnit === 'f') {
+    if (toUnit === 'c') return (value - 32) * 5 / 9;
+    if (toUnit === 'k') return (value + 459.67) * 5 / 9;
+    return value;
+  }
+  if (fromUnit === 'k') {
+    if (toUnit === 'c') return value - 273.15;
+    if (toUnit === 'f') return value * 9 / 5 - 459.67;
+    return value;
+  }
+  throw new Error('Invalid unit');
+}
+
+form.addEventListener('input', () => {
+  const inputTemp = parseFloat(inputField.value);
+  const fromUnit = fromUnitField.value;
+  const toUnit = toUnitField.value;
+  const outputTemp = convertTemp(inputTemp, fromUnit, toUnit);
+  if (!isNaN(outputTemp))
+    outputField.value = (Math.round(outputTemp * 100) / 100) + ' ' + toUnit.toUpperCase();
+  else
+    outputField.value = 'Invalid';
+});
